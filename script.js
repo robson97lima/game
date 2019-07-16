d45e4f6b0508680b4c7b6d416af13d69445293b7
robson97lima <robson97lima@gmail.com>
2019-07-16 11:44:16 -0300

    Fazer com base no professor

---
+++ script.js
@@ -0,0 +1,17 @@
+let imagensSalvas = ["Bowser.jpg","egman.jpg","Knuckles_9.png","Luigi.jpg","Mario.jpg","sha.jpg","Sonic.jpg","tails.png"];
+let imagens = imagensSalvas.concat(imagensSalvas);
+function embaralhar(lista){
+    let valor_temporario;
+    let indice_aleatorio;
+for (let i = lista.length -1; i !== 0; i--){
+    indice_aleatorio - Math.floor(Math.random()*i);
+    valor_temporario = lista[i];
+    lista[i] = lista 
+}
+
+)
+}
+imagens = embaralhar
+for (let i in cartas){
+    
+}
